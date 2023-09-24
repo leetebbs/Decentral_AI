f09c6c06be07338e19cc4e7a3af56078fe133961
Tebbo <leetebbs@ymail.com>
2023-09-24 15:28:12 +0100

    Backend

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
 const express = require('express');
-const dotenv = require("dotenv").config();
+const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
 
 
 //parser for body data
 app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 
-// app.use('/openai', require('./routes/openaiRoutes'));
+app.use('/openai', require('./routes/openaiRoutes'));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
-
-
-app.post('/generateimage', (req, res) => {
-    generateimage(req);
-})
-
-
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
