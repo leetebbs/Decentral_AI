3daf0cab9acf0b73d05ecad1d249dfcae3280505
Tebbo <leetebbs@ymail.com>
2023-09-24 15:26:18 +0100

    Backend

--- index.js
+++ index.js
@@ -14,42 +14,9 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
-async function generateimage(req, res) {
-    
-
-    const { prompt, size } = req.body;
-    const imageSize =
-      size === "small" ? "256x256" : size === "medium" ? "512x512" : "1024x1024";
-    try {
-      const response = await openai.images.generate({
-        prompt,
-        n: 1,
-        size: imageSize,
-      });
-  
-      const imageUrl = response.data[0].url;
-  
-      res.status(200).json({
-        success: true,
-        data: imageUrl,
-      });
-    } catch (error) {
-      res.status(400).json({
-        success: false,
-        error: "Image could not be generated",
-      });
-      if (error.response) {
-        console.log(error.response.status);
-        console.log(error.response.data);
-      } else {
-        console.log(error.message);
-      }
-    }
-}
-
 
 app.post('/generateimage', (req, res) => {
-    generateimage(req, res);
+    generateimage(req);
 })
 
 
