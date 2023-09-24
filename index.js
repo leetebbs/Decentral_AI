db77b1dce9d328ef565562cb16d086edb3f12c49
Tebbo <leetebbs@ymail.com>
2023-09-24 15:41:08 +0100

    Backend

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 const app = express();
 
-
+app.use(cors())
 //parser for body data
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
